546aa6bc1f148c8408262ffdfc9f525bce08c378
Pablo <pablo.aqm@gmail.com>
2024-01-29 20:12:54 -0300

    feat: angular material installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideStore()]
+  providers: [provideRouter(routes), provideClientHydration(), provideStore(), provideAnimations()]
 };
