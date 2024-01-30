aded0d9373ba0bf0be25896feee0a6551cb15212
Pablo <pablo.aqm@gmail.com>
2024-01-29 23:43:25 -0300

    feat: main arquitecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'core',
+        loadComponent: () => import('./core/core.component').then(m => m.CoreComponent)
+    }
+];
